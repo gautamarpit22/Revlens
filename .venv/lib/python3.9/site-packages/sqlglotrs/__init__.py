937775b07da259f7163022cac1bf07f587828b43
Arpit Gautam <gautamarpit9@gmail.com>
2026-07-20 10:38:28 +0530

    Remove virtualenv from repo (gitignore fix)

--- .venv/lib/python3.9/site-packages/sqlglotrs/__init__.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-warnings.warn(
-    "sqlglotrs is deprecated and no longer maintained. "
-    "Please use sqlglotc instead for faster parsing: pip install sqlglotc",
-    DeprecationWarning,
-)
